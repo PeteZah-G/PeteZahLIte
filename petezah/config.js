18d9bef02694bdcdd6e18d760c2359864584274a
PeteZah <petezahiscool@gmail.com>
2025-05-22 20:38:10 +0000

    Update config.js

--- petezah/config.js
+++ petezah/config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/petezah/petezah/",
-  bare: "/bare/",
+  bare: "https://petezahserver.adidasnmdcitysock.com/seal/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/petezah/handler.js",
